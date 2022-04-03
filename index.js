a907645215bc16c361b947d7908fbd2c9e995c5e
Richard H Lee <ricardohenrylee@gmail.com>
2022-04-03 21:57:06 +0100

    animations

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ setTimeout(
   () => {
     minutes.addEventListener(
       "keyup",
-      event => {
-        if (event.key === "Enter")
-          alert(123);
-      }
+      event => {if (event.key === "Enter") {
+        document.body.classList.add("active");
+        document.documentElement.style.setProperty("--time", minutes.value +  "s");
+        minutes.remove();
+      }}
     );
   },
   0
