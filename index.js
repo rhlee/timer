f44f145b2c579dde96e3cbcd662eabcadcb267a1
Richard H Lee <ricardohenrylee@gmail.com>
2022-08-16 19:05:55 +0100

    wakelock check

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const factor = 10 / 100;
 
-setTimeout(
-  () => {
-    minutes.addEventListener(
-      "keyup",
-      event => {if (event.key === "Enter") {
-        document.body.classList.add("active");
-        const seconds = minutes.value * 60;
-        const factorSeconds = () => seconds * factor * Math.random();
-        const rootStyle = document.documentElement.style;
-        rootStyle.setProperty("--time", (seconds - factorSeconds()) +  "s");
-        rootStyle.setProperty("--duration", (factorSeconds() * 2) +  "s");
-        minutes.remove();
-        document.documentElement.requestFullscreen();
-      }}
-    );
-  },
-  0
-);
+if ('wakeLock' in navigator)
+  setTimeout(
+    () => {
+      minutes.addEventListener(
+        'keyup',
+        event => {if (event.key === 'Enter') {
+          document.body.classList.add('active');
+          const seconds = minutes.value * 60;
+          const factorSeconds = () => seconds * factor * Math.random();
+          const rootStyle = document.documentElement.style;
+          rootStyle.setProperty('--time', (seconds - factorSeconds()) +  's');
+          rootStyle.setProperty('--duration', (factorSeconds() * 2) +  's');
+          minutes.remove();
+          document.documentElement.requestFullscreen();
+        }}
+      );
+    },
+    0
+  );
+else
+  alert("wakelock not supported");
