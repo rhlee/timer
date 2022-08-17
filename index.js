d8a0708f111058094e828777cf8ea6201d5f0f56
Richard H Lee <ricardohenrylee@gmail.com>
2022-08-17 12:58:55 +0100

    request wakelock

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const factor = 10 / 100;
 
-if ('wakeLock' in navigator)
-  setTimeout(
-    () => {
-      minutes.addEventListener(
-        'keyup',
-        event => {if (event.key === 'Enter') {
-          document.body.classList.add('active');
-          const seconds = minutes.value * 60;
-          const factorSeconds = () => seconds * factor * Math.random();
-          const rootStyle = document.documentElement.style;
-          rootStyle.setProperty('--time', (seconds - factorSeconds()) +  's');
-          rootStyle.setProperty('--duration', (factorSeconds() * 2) +  's');
-          minutes.remove();
-          document.documentElement.requestFullscreen();
-        }}
-      );
-    },
-    0
-  );
-else
-  alert("wakelock not supported");
+if ('wakeLock' in navigator) navigator.wakeLock.request('screen')
+  .then(
+    () => setTimeout(
+      () => {
+        minutes.addEventListener(
+          'keyup',
+          event => { if (event.key === 'Enter') {
+            document.body.classList.add('active');
+            const seconds = minutes.value * 60;
+            const factorSeconds = () => seconds * factor * Math.random();
+            const rootStyle = document.documentElement.style;
+            rootStyle
+              .setProperty('--time', (seconds - factorSeconds()) +  's');
+            rootStyle.setProperty('--duration', (factorSeconds() * 2) +  's');
+            minutes.remove();
+            document.documentElement.requestFullscreen();
+          } }
+        );
+      },
+      0
+    )
+  ).catch(alert);
+else alert("wakelock not supported");
